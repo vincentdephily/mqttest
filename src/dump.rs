d663326b76beee1634c1534f038570738dd14dac
Vincent de Phily <vincent.dephily@mobile-devices.fr>
2019-09-18 16:39:28 +0100

    feat: Complete json serialization of all mqtt packets, not just con/conack/pub.
    This is a fair amount of duplication from the upstream mqttrs structs, but it serves two purposes
    (json serde, and some simplifications) and is the path of least resistance for the moment.

--- src/dump.rs
+++ src/dump.rs
@@ -36,17 +36,36 @@ pub struct DumpMeta<'a> {
 //    time::strptime(&s, "%FT%T.").map_err(serde::de::Error::custom)
 //}
 
+pub type DumpPid = u16;
+
+/// Parsed QoS.
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub enum DumpQos {
+    AtMostOnce,
+    AtLeastOnce,
+    ExactlyOnce,
+}
+impl From<QoS> for DumpQos {
+    fn from(q: QoS) -> Self {
+        match q {
+            QoS::AtMostOnce => Self::AtMostOnce,
+            QoS::AtLeastOnce => Self::AtLeastOnce,
+            QoS::ExactlyOnce => Self::ExactlyOnce,
+        }
+    }
+}
+
 /// Parsed QoS+PacketIdentifier. Note that the identifier MUST be absent for AtMostOnce and present
 /// for AtLeastOnce/ExactlyOnce.
 // FIXME: Instead of panicing, return a result and let `DumpMqtt::from(&Packet)` fail cleanly just
 // for this client and/or dump a `DumpMqtt::Invalid` variant.
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
-pub enum DumpQoS {
+pub enum DumpQosId {
     AtMostOnce,
-    AtLeastOnce(u16),
-    ExactlyOnce(u16),
+    AtLeastOnce(DumpPid),
+    ExactlyOnce(DumpPid),
 }
-impl DumpQoS {
+impl DumpQosId {
     fn from(q: QoS, p: Option<PacketIdentifier>) -> Self {
         match (q, p) {
             (QoS::AtMostOnce, None) => Self::AtMostOnce,
@@ -57,12 +76,18 @@ impl DumpQoS {
     }
 }
 
+/// Parsed MQTT connack packet.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DumpConnack {
+    session: bool,
+    code: u8, //FIXME: Proper names
+}
+
 /// Parsed MQTT publish packet.
-// FIXME: This currently relies on a forked `mqttrs` with serde derives.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct DumpPublish {
     pub dup: bool,
-    pub qos: DumpQoS,
+    pub qos: DumpQosId,
     pub topic: String,
     /// The payload as a string, if it is valid utf-8
     pub utf8: Option<String>,
@@ -70,22 +95,58 @@ pub struct DumpPublish {
     pub bytes: Vec<u8>,
 }
 
-/// Parsed MQTT packet. We use our own struct instead `mqttrs::Packet` so that we can implement json
-/// serialisation, and add/remove some fields for readbility/unit-testing.
+/// Parsed MQTT subscribe packet.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DumpSubscribe {
+    pub pid: DumpPid,
+    pub topics: Vec<DumpSubscribeTopic>,
+}
+
+/// Parsed MQTT unsubscribe packet.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DumpUnsubscribe {
+    pub pid: DumpPid,
+    pub topics: Vec<String>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DumpSubscribeTopic {
+    pub topic: String,
+    pub qos: DumpQos,
+}
+
+/// Parsed MQTT suback packet.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DumpSuback {
+    pub pid: DumpPid,
+    pub codes: Vec<DumpSubackcode>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum DumpSubackcode {
+    AtMostOnce,
+    AtLeastOnce,
+    ExactlyOnce,
+    Failure,
+}
+
+
+/// Parsed MQTT packet. We use our own struct and substructs instead of the `mqttrs` ones, so that
+/// we can implement json serialisation, and add/remove some fields for readbility/unit-testing.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum DumpMqtt {
     /// The string is the client id.
     Connect(String),
-    Connack,
+    Connack(DumpConnack),
     Publish(DumpPublish),
-    Puback,
-    Pubrec,
-    Pubrel,
-    PubComp,
-    Subscribe,
-    SubAck,
-    UnSubscribe,
-    UnSubAck,
+    Puback(DumpPid),
+    Pubrec(DumpPid),
+    Pubrel(DumpPid),
+    PubComp(DumpPid),
+    Subscribe(DumpSubscribe),
+    SubAck(DumpSuback),
+    UnSubscribe(DumpUnsubscribe),
+    UnSubAck(DumpPid),
     PingReq,
     PingResp,
     Disconnect,
@@ -93,23 +154,51 @@ pub enum DumpMqtt {
 impl From<&Packet> for DumpMqtt {
     fn from(p: &Packet) -> Self {
         match p {
-            Packet::Connect(c) => Self::Connect(c.client_id.clone()),
-            Packet::Connack(_) => Self::Connack,
+            Packet::Connect(p) => Self::Connect(p.client_id.clone()),
+            Packet::Connack(p) => {
+                Self::Connack(DumpConnack { session: p.session_present, code: p.code.to_u8() })
+            },
             Packet::Publish(p) => {
                 Self::Publish(DumpPublish { dup: p.dup,
-                                            qos: DumpQoS::from(p.qos, p.pid),
+                                            qos: DumpQosId::from(p.qos, p.pid),
                                             topic: p.topic_name.clone(),
                                             utf8: String::from_utf8(p.payload.clone()).ok(),
                                             bytes: p.payload.clone() })
             },
-            Packet::Puback(_) => Self::Puback,
-            Packet::Pubrec(_) => Self::Pubrec,
-            Packet::Pubrel(_) => Self::Pubrel,
-            Packet::PubComp(_) => Self::PubComp,
-            Packet::Subscribe(_) => Self::Subscribe,
-            Packet::SubAck(_) => Self::SubAck,
-            Packet::UnSubscribe(_) => Self::UnSubscribe,
-            Packet::UnSubAck(_) => Self::UnSubAck,
+            Packet::Puback(p) => Self::Puback(p.0),
+            Packet::Pubrec(p) => Self::Pubrec(p.0),
+            Packet::Pubrel(p) => Self::Pubrel(p.0),
+            Packet::PubComp(p) => Self::PubComp(p.0),
+            Packet::Subscribe(p) => {
+                let topics =
+                    p.topics
+                     .iter()
+                     .map(|s| DumpSubscribeTopic { topic: s.topic_path.clone(), qos: s.qos.into() })
+                     .collect();
+                Self::Subscribe(DumpSubscribe { pid: p.pid.0, topics })
+            },
+            Packet::SubAck(p) => {
+                let codes = p.return_codes
+                             .iter()
+                             .map(|c| match c {
+                                 SubscribeReturnCodes::Success(QoS::AtMostOnce) => {
+                                     DumpSubackcode::AtMostOnce
+                                 },
+                                 SubscribeReturnCodes::Success(QoS::AtLeastOnce) => {
+                                     DumpSubackcode::AtLeastOnce
+                                 },
+                                 SubscribeReturnCodes::Success(QoS::ExactlyOnce) => {
+                                     DumpSubackcode::ExactlyOnce
+                                 },
+                                 SubscribeReturnCodes::Failure => DumpSubackcode::Failure,
+                             })
+                             .collect();
+                Self::SubAck(DumpSuback { pid: p.pid.0, codes })
+            },
+            Packet::UnSubscribe(p) => {
+                Self::UnSubscribe(DumpUnsubscribe { pid: p.pid.0, topics: p.topics.clone() })
+            },
+            Packet::UnSubAck(p) => Self::UnSubAck(p.0),
             Packet::PingReq => Self::PingReq,
             Packet::PingResp => Self::PingResp,
             Packet::Disconnect => Self::Disconnect,
